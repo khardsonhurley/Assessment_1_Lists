aebabf291ccebf621febe00884e52b915f485a51
Krishelle Hardson-Hurley <khardsonhurley@Krishelles-MacBook-Pro.local>
2016-07-11 13:51:50 -0700

    Week 1: Practice and Skills Assessment

---
+++ assessment.py
@@ -0,0 +1,149 @@
+"""List Assessment 
+
+Edit the functions until all of the doctests pass when
+you run this file.
+"""
+
+
+def all_odd(numbers):
+    """Return a list of only the odd numbers in the input list.
+
+    For example::
+
+        >>> all_odd([1, 2, 7, -5])
+        [1, 7, -5]
+
+        >>> all_odd([2, -6, 8])
+        []
+    """
+    odds = []
+    for number in numbers:
+        if number % 2 == 1:
+            odds.append(number)
+    return odds
+
+
+
+
+def print_indices(items):
+    """Print index of each item in list, followed by item itself.
+
+    Do this without using a "counting variable" --- that is, don't
+    do something like this::
+
+        count = 0
+        for item in list:
+            print count
+            count = count + 1
+
+    Output should look like this::
+
+        >>> print_indices(["Toyota", "Jeep", "Volvo"])
+        0 Toyota
+        1 Jeep
+        2 Volvo
+    """
+    length = len(items)
+    index_list = range(length) 
+    my_string = ""
+
+    for index in index_list:
+        my_string = str(index) + " "
+        print my_string + items[index]
+
+
+def foods_in_common(foods1, foods2):
+    """Find foods in common.
+
+    Given 2 lists of foods, return the items that are in common
+    between the two, sorted alphabetically.
+
+    **NOTE**: for this problem, you're welcome to use any of the
+    Python data structures you've been introduced to (not just
+    lists). Is there another that would be a good idea?
+
+    For example::
+
+        >>> foods_in_common(
+        ...     ["cheese", "bagel", "cake", "kale"],
+        ...     ["hummus", "beets", "bagel", "lentils", "kale"]
+        ... )
+        ['bagel', 'kale']
+
+    If there are no foods in common, return an empty list::
+
+        >>> foods_in_common(
+        ...     ["lamb", "chili", "cheese"],
+        ...     ["cake", "ice cream"]
+        ... )
+        []
+
+    """
+    set_1 = set(foods1)
+    set_2 = set(foods2)
+    common_foods = set_1 & set_2
+
+    return list(common_foods)
+
+
+def every_other_item(items):
+    """Return every other item in `items`, starting at first item.
+
+    For example::
+
+       >>> every_other_item([1, 2, 3, 4, 5, 6])
+       [1, 3, 5]
+
+       >>> every_other_item(
+       ...   ["you", "z", "are", "z", "good", "z", "at", "x", "code"]
+       ... )
+       ['you', 'are', 'good', 'at', 'code']
+    """
+
+    odds = []
+    count = 0
+    for item in items:
+        if count % 2 == 0:
+            odds.append(item)
+        count = count + 1
+    return odds
+
+def largest_n_items(items, n):
+    """Return the `n` largest integers in list, in ascending order.
+
+    You can assume that `n` will be less than the length of the list.
+
+    For example::
+
+        >>> largest_n_items([2, 6006, 700, 42, 6, 59], 3)
+        [59, 700, 6006]
+
+    It should work when `n` is 0::
+
+        >>> largest_n_items([3, 4, 5], 0)
+        []
+
+    If there are duplicates in the list, they should be counted
+    separately::
+
+        >>> largest_n_items([3, 3, 3, 2, 1], 2)
+        [3, 3]
+    """
+    items.sort()
+    if n == 0:
+        return []
+    else:
+        return items[-n:]
+
+
+#####################################################################
+# END OF ASSESSMENT: You can ignore everything below.
+
+if __name__ == "__main__":
+    import doctest
+
+    print
+    result = doctest.testmod()
+    if not result.failed:
+        print "ALL TESTS PASSED. GOOD WORK!"
+    print
